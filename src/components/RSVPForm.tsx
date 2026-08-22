5852c3d11287aba4de366fae737549f3d0d137e6
lavanyacheshani <lavanyachashani5@gmail.com>
2026-08-23 02:54:20 +0530

    Update for Warsha & Mennan

--- src/components/RSVPForm.tsx
+++ src/components/RSVPForm.tsx
@@ -89,11 +89,7 @@ export const RSVPForm: React.FC<RSVPFormProps> = ({ inviteeName = '', eventName 
               : `Your presence means the world to us. Please kindly let us know if you will be able to join our celebration.`
             }
           </p>
-          <div className="mt-6 mb-8 text-sm font-sans tracking-[0.2em] uppercase font-semibold text-brand-plum drop-shadow-sm leading-loose">
-            RSVP BY <br />
-            076 429 1756 - Warsha <br /> 
-            076 533 9931 - Mennan
-          </div>
+
           <div className="w-12 h-[1px] bg-brand-lavender/50 mx-auto lg:mx-0" />
         </div>
 

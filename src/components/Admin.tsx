00e896dac7edffd374a1faa0d7f26cd823623b6e
lavanyacheshani <lavanyachashani5@gmail.com>
2026-09-09 00:27:14 +0530

    hh

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -52,7 +52,7 @@ ${url}
 Your presence would truly mean the world to us, and we would be honored to celebrate this beautiful moment together.
 
 With love,
-❤️ Mennan & Warsha`;
+❤️ Warsha & Mennan`;
   };
 
   const handleCopyMessageActive = () => {
